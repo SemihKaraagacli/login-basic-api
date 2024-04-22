caab5fc10f7dd9222104068e0f3abb3a8b403007
Lumos <karaagaclisemih@gmail.com>
2024-04-22 19:30:43 +0300

    todo basic api

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LoginApi.Models;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Authorization;
+
+namespace LoginApi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        [HttpPost]
+        public Responses Login(Users users)
+        {
+            if (users.username == "semih" && users.password == "123")
+            {
+                return new Responses()
+                {
+                    Message = "Login işlemi başarılı.",
+                    Code = "200",
+                    Data = null
+                };
+            }
+
+            return new Responses()
+            {
+                Message = "Login işlemi başarısız.",
+                Code = "401",
+                Data = null
+            };
+
+        }
+
+    }
+}
---
+++ Models/Responses.cs
@@ -0,0 +1,9 @@
+﻿namespace LoginApi.Models
+{
+    public class Responses
+    {
+        public string Message { get; set; }
+        public string Code { get; set; }
+        public object Data { get; set; }
+    }
+}
